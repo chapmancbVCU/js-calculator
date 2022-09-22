02249090e782441692351cd75809de841b2f87eb
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-21 20:30:00 -0400

    Created comments section describing each file.

---
+++ js/calculator.js
@@ -0,0 +1,6 @@
+/******************************************************************************
+ *         Name: calculator.js
+ *       Author: Chad Chapman
+ * Date Created: September 21, 2022
+ *  Description: Implementation for calculator.
+******************************************************************************/
