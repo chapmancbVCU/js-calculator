ceee6da0b4bf226ea3886126dd09297af6b49d84
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-23 19:05:46 -0400

    Added styling to output text.  Added code to concatenate previous input with current input.

--- js/calculator.js
+++ js/calculator.js
@@ -5,10 +5,17 @@
  *  Description: Implementation for calculator.
 ******************************************************************************/
 
+/******************************************************************************
+ * GLOBAL VARIABLES
+ *****************************************************************************/
+let value = "";
+
 // Setup event listener for button click.
 const buttonSelect = document.querySelectorAll('button');
 buttonSelect.forEach(button => button.addEventListener('click', function() {
-    const value = this.getAttribute('value');
+    let newValue = this.getAttribute('value');
+    console.log(`value: ${value}`);
+    value += newValue;
     document.getElementById('output').innerHTML = value;
 }));
 
