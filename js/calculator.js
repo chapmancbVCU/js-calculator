72e20e252443447cafc04a51bcc2bea6ef86a295
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-23 18:43:08 -0400

    Added event listener that prints associated with button to console and output on calculator.

--- js/calculator.js
+++ js/calculator.js
@@ -4,3 +4,11 @@
  * Date Created: September 21, 2022
  *  Description: Implementation for calculator.
 ******************************************************************************/
+
+// Setup event listener for button click.
+const buttonSelect = document.querySelectorAll('button');
+buttonSelect.forEach(button => button.addEventListener('click', function() {
+    const value = this.getAttribute('value');
+    document.getElementById('output').innerHTML = value;
+}));
+
