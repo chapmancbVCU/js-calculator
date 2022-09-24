a617610ebe7d49a9003fc31980b266b8db0bc6a8
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-23 20:11:58 -0400

    Did more testing and writing text code.

--- js/calculator.js
+++ js/calculator.js
@@ -9,13 +9,33 @@
  * GLOBAL VARIABLES
  *****************************************************************************/
 let value = "";
+let firstOperand = "";
+let secondOperand = "";
+
 
 // Setup event listener for button click.
 const buttonSelect = document.querySelectorAll('button');
 buttonSelect.forEach(button => button.addEventListener('click', function() {
     let newValue = this.getAttribute('value');
     console.log(`value: ${value}`);
-    value += newValue;
-    document.getElementById('output').innerHTML = value;
+    
+    // Limite size of operand to 9 digits.
+    if(value.length < 9) {
+        if(newValue == "+/-") {
+            value = setPositiveOrNegative(value, newValue);
+            console.log(newValue);
+        } else {
+            value += newValue;
+        }
+        document.getElementById('output').innerHTML = value;
+    }
 }));
 
+function setPositiveOrNegative(currentValue, newValue) {
+    if(currentValue[0] === "-") {
+        return currentValue.substring(1, currentValue.length);
+    }
+    else {
+        return "-" + currentValue;
+    }
+}
